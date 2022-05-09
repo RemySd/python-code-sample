b4cde5f368b01d0fd986589d04daeb283ecdbf4f
Remy <remy.sainsard@gmail.com>
2022-05-09 16:20:50 +0200

    Code clean

--- pandas_tool/vwap.py
+++ pandas_tool/vwap.py
@@ -4,16 +4,8 @@ df = pd.read_csv("sample.csv")
 
 times = pd.to_datetime(df.timestamp)
 
-print(times.dt.hour)
-
-df = df.groupby([times.dt.hour]).agg({'price':'sum ','amount':'sum'})
-print(df)
-print("---")
-print(df.price * df.amount)
+df = df.groupby([times.dt.hour]).agg({'price':'sum','amount':'sum'})
 
 df["vwap"] = df.price / df.amount
 
-print("---", df)
-
-#for index, row in df.iterrows():
-#    print(pandas.to_datetime(row['timestamp']))
+print(df)
